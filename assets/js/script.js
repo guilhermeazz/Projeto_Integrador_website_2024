e28586d0bfd0eb61f0a7518c05920ed8a7fc4802
Guilherme Albuquerque <guialbuzapa@gmail.com>
2024-05-02 14:27:26 -0300

    feature: atualização de telas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,20 @@
-console.log("teste");
+function toggleMenu() {
+    var menu = document.getElementById("profileMenu");
+    if (menu.style.display === "none") {
+      menu.style.display = "block";
+    } else {
+      menu.style.display = "none";
+    }
+  }
+  
+  // Fechar o menu se clicar fora dele
+  window.onclick = function(event) {
+    if (!event.target.closest('.profile')) {
+      var menus = document.getElementsByClassName("profile-menu");
+      for (var i = 0; i < menus.length; i++) {
+        var openMenu = menus[i];
+        openMenu.style.display = "none";
+      }
+    }
+  }
+  
